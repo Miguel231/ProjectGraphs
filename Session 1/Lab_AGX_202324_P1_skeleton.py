63efc47dbd58537d385028f72e9e432c04b72b78
Miguel231 <miguelpitufet@gmail.com>
2024-06-06 13:21:41 +0200

    part 1 acabado con comments

--- Session 1/Lab_AGX_202324_P1_skeleton.py
+++ Session 1/Lab_AGX_202324_P1_skeleton.py
@@ -12,9 +12,9 @@ def search_artist(sp: spotipy.client.Spotify, artist_name: str) -> str:
     :param artist_name: Name of the artist to search for.
     :return: Spotify artist ID.
     """
-    artist = sp.search(q='artist:' + artist_name, type='artist')
-    items = artist['artists']['items']
-    return items[0]['id']
+    artist = sp.search(q='artist:' + artist_name, type='artist') #Perform a search for the artist
+    items = artist['artists']['items']  #list of found artists
+    return items[0]['id'] #Return the ID of the first artist of the list
 
 def crawler(sp: spotipy.client.Spotify, seed: str, max_nodes_to_crawl: int, strategy: str = "BFS",
             out_filename: str = "g.graphml") -> nx.DiGraph:
@@ -30,13 +30,14 @@ def crawler(sp: spotipy.client.Spotify, seed: str, max_nodes_to_crawl: int, stra
     """
     graph = nx.DiGraph()
     queue = [seed]
-    visited = set()
+    visited = set() #Track visited nodes
     
-    while queue and len(visited) < max_nodes_to_crawl:
+    while queue and len(visited) < max_nodes_to_crawl: #until it's empty or max node is reached
         current_id = queue.pop(0) if strategy == "BFS" else queue.pop()
         if current_id not in visited:
-            visited.add(current_id)
+            visited.add(current_id) #visited
             artist_info = sp.artist(current_id)
+            #add artist as a node with information
             graph.add_node(current_id, name=artist_info['name'], followers=artist_info['followers']['total'],
                            popularity=artist_info['popularity'], genres=", ".join(artist_info['genres']))
             related_artists = sp.artist_related_artists(current_id)['artists'] # all related artist from a curent artist
@@ -61,10 +62,10 @@ def get_track_data(sp: spotipy.client.Spotify, graphs: list, out_filename: str) 
     data = []
     for graph in graphs:
         for artist_id in graph.nodes:
-            tracks = sp.artist_top_tracks(artist_id, country='ES')['tracks']
+            tracks = sp.artist_top_tracks(artist_id, country='ES')['tracks'] #Get top tracks for the artist (Spain)
             for track in tracks:
                 track_id = track['id']
-                audio_features = sp.audio_features(track_id)[0]
+                audio_features = sp.audio_features(track_id)[0] #audio features for the track
                 album = track['album']
                 data.append({
                     'Artist ID': artist_id,
@@ -87,15 +88,13 @@ def get_track_data(sp: spotipy.client.Spotify, graphs: list, out_filename: str) 
                     'Album Release Date': album['release_date']
                 })
     
-    trackdata = pd.DataFrame(data)
+    trackdata = pd.DataFrame(data) #list of dictionaries to a DataFrame
     trackdata.to_csv(out_filename, index=False)
     return trackdata
 
 if __name__ == "__main__":
-
     '''
-
-    # Search for Taylor Swift's artist ID
+    #search for Taylor Swift's artist ID
     taylor_swift = search_artist(cr.sp, "Taylor Swift")
     
     # Part a) Crawl using BFS
@@ -111,19 +110,14 @@ if __name__ == "__main__":
     g = [gB.subgraph(artists)]
     D = get_track_data(cr.sp, g, "Session 1/songs.csv")
 
-    # Part d) Additional crawling for another artist (example: Pastel Ghost)
+    # Part d) Crawling for another artist -> Pastel Ghost
     pastel_ghost = search_artist(cr.sp, "Pastel Ghost")
-
-    hb = crawler(cr.sp, pastel_ghost, max_nodes_to_crawl=100, strategy="BFS", out_filename="hB.graphml")
-
-    
-    # Reading generated GraphML files and calculating statistics
 
     hb = crawler(cr.sp, pastel_ghost, max_nodes_to_crawl=100, strategy="BFS", out_filename="Session 1/hB.graphml")
     '''
     
     #EXERCISE 1
-
+    #Read generated GraphML
     gB = nx.read_graphml("Session 1/gB.graphml")
     gD = nx.read_graphml("Session 1/gD.graphml")
 
@@ -132,37 +126,57 @@ if __name__ == "__main__":
     order_gD = gD.number_of_nodes()
     size_gD = gD.number_of_edges()
 
-    print(f"Order gB: {order_gB}, Size gB: {size_gB}")
+    #Order and Size of both graphs
+    print(f"\nOrder gB: {order_gB}, Size gB: {size_gB}")
     print(f"Order gD: {order_gD}, Size gD: {size_gD}")
 
     #EXERCISE 2
+    #Calculate in-degrees and out-degrees for gB
     in_degrees_gB = [d for n, d in gB.in_degree()]
     out_degrees_gB = [d for n, d in gB.out_degree()]
+    #The same for gD
     in_degrees_gD = [d for n, d in gD.in_degree()]
     out_degrees_gD = [d for n, d in gD.out_degree()]
 
+    #print min, max and median for each graph
     stats_gB = {
-        'in_degree': {'min': np.min(in_degrees_gB), 'max': np.max(in_degrees_gB), 'median': np.median(in_degrees_gB)},
-        'out_degree': {'min': np.min(out_degrees_gB), 'max': np.max(out_degrees_gB), 'median': np.median(out_degrees_gB)}
+        'in_degree': {
+            'min': np.min(in_degrees_gB), 
+            'max': np.max(in_degrees_gB), 
+            'median': np.median(in_degrees_gB)
+        },
+        'out_degree': {
+            'min': np.min(out_degrees_gB),
+            'max': np.max(out_degrees_gB), 
+            'median': np.median(out_degrees_gB)
+        }
     }
 
     stats_gD = {
-        'in_degree': {'min': np.min(in_degrees_gD), 'max': np.max(in_degrees_gD), 'median': np.median(in_degrees_gD)},
-        'out_degree': {'min': np.min(out_degrees_gD), 'max': np.max(out_degrees_gD), 'median': np.median(out_degrees_gD)}
+        'in_degree': {
+            'min': np.min(in_degrees_gD), 
+            'max': np.max(in_degrees_gD), 
+            'median': np.median(in_degrees_gD)
+        },
+        'out_degree': {
+            'min': np.min(out_degrees_gD), 
+            'max': np.max(out_degrees_gD), 
+            'median': np.median(out_degrees_gD)
+        }
     }
 
-    print(f"gB In-Degree: {stats_gB['in_degree']}, Out-Degree: {stats_gB['out_degree']}")
-    print(f"gD In-Degree: {stats_gD['in_degree']}, Out-Degree: {stats_gD['out_degree']}")
+    print(f"\ngB -> \nIn-Degree: {stats_gB['in_degree']}, Out-Degree: {stats_gB['out_degree']}")
+    print(f"gD -> \nIn-Degree: {stats_gD['in_degree']}, Out-Degree: {stats_gD['out_degree']}")
 
     #EXERCISE 3
     D = pd.read_csv("Session 1/songs.csv")
+    #number of songs
     num_songs = D.shape[0]
+    #unique artists
     num_artists = D['Artist ID'].nunique()
+    #unique albums
     num_albums = D['Track Name'].nunique()
 
-    print(f"Num of songs: {num_songs}")
+    print(f"\nNum of songs: {num_songs}")
     print(f"Num of artists: {num_artists}")
     print(f"Num of albums: {num_albums}")
-
-os.chdir('C:/GitHub Repositories/ProjectGraphs/Session 1')
-
